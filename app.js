e855a93bbf0d50a87121b5f3341083b517c48d6d
Jeremy Lyons <jlyons210@gmail.com>
2023-04-17 19:35:14 -0500

    Releasing v0.4.0 with gpt-3.5-turbo support. Do not use older models.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
 
-// TODO: Dirty quit application if config isn't populated
+// Ensure all required environment variables are set
 checkEnvironment();
 
 // Create Discord client
@@ -32,7 +32,7 @@ discordClient.once(Events.ClientReady, c => {
 // Discord channel message listener
 discordClient.on(Events.MessageCreate, async message => {
   // If the incoming message does not @-mention bot, return
-  if (!discordClient.user?.id) {
+  if (!discordClient.user.id) {
     log('discordClient.user.id is not set.', 'error');
     return;
   }
@@ -54,25 +54,42 @@ discordClient.on(Events.MessageCreate, async message => {
 async function askChatGPT(prompt) {
 
   try {
-  // Send prompt to OpenAI API
-    log(`New OpenAI prompt: ${prompt}`, 'info');
+    log(`OpenAI prompt: ${prompt}`, 'info');
 
-    const completion = await openAiClient.createCompletion({
+    // Construct message payload
+    const messages = [
+      {
+        role: 'system',
+        content: process.env.OPENAI_PARAM_SYSTEM_PROMPT,
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ];
+
+    // Send payload to OpenAI API
+    const completion = await openAiClient.createChatCompletion({
       max_tokens: parseInt(process.env.OPENAI_PARAM_MAX_TOKENS),
       model: process.env.OPENAI_PARAM_MODEL,
-      prompt: prompt,
+      messages: messages,
       temperature: parseFloat(process.env.OPENAI_PARAM_TEMPERATURE),
     });
 
-    let response = completion.data.choices[0].text.trim();
+    // Assign response
+    let response = completion.data.choices[0].message.content.trim();
 
+    // Sometimes an empty response comes back if the prompt is garbage
+    // You can't publish an empty message to Discord's API
     if (response == '' && completion.status == 200) {
       const tryAgainResponses = [
         'I don\'t know what you mean.',
         'Use your words.',
         'That doesn\'t make any sense.',
+        'Could you repeat that, but better?',
       ];
 
+      // Pick a random tryAgainResponse
       const tryAgainResponse = Math.floor(Math.random() * tryAgainResponses.length);
       response = tryAgainResponses[tryAgainResponse];
     }
@@ -98,11 +115,13 @@ function checkEnvironment() {
     'OPENAI_API_KEY',
     'OPENAI_PARAM_MAX_TOKENS',
     'OPENAI_PARAM_MODEL',
+    'OPENAI_PARAM_SYSTEM_PROMPT',
     'OPENAI_PARAM_TEMPERATURE',
   ];
 
   let quitError = false;
 
+  // Check that each required environment variable is set
   requiredEnvVars.forEach(envVar => {
     if (!process.env[envVar]) {
       log(`Environment variable not set: ${envVar}`, 'error');
@@ -110,6 +129,7 @@ function checkEnvironment() {
     }
   });
 
+  // Quit with uncaught error if any environment variable is not set
   if (quitError) {
     log('Environment variables are not configured correctly. See documentation on GitHub.', 'error');
     throw (new Error('Configuration error exit.'));
