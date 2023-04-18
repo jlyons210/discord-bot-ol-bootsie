31c08fdd417eb69b9434a9065091b903809f2ffb
Jeremy Lyons <jlyons210@gmail.com>
2023-04-17 20:08:31 -0500

    Restored 0.4.0 from another machine

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Import modules
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
+const util = require('util');
 
-// TODO: Dirty quit application if config isn't populated
+// Ensure all required environment variables are set
 checkEnvironment();
 
 // Create Discord client
@@ -32,7 +33,7 @@ discordClient.once(Events.ClientReady, c => {
 // Discord channel message listener
 discordClient.on(Events.MessageCreate, async message => {
   // If the incoming message does not @-mention bot, return
-  if (!discordClient.user?.id) {
+  if (!discordClient.user.id) {
     log('discordClient.user.id is not set.', 'error');
     return;
   }
@@ -52,33 +53,61 @@ discordClient.on(Events.MessageCreate, async message => {
 
 // Poll OpenAI API
 async function askChatGPT(prompt) {
+  log(`OpenAI prompt: ${prompt}`, 'info');
+
+  // Construct message payload
+  const messages = [
+    {
+      role: 'system',
+      content: process.env.OPENAI_PARAM_SYSTEM_PROMPT,
+    },
+    {
+      role: 'user',
+      content: prompt,
+    },
+  ];
+
+  let response = '';
 
   try {
-  // Send prompt to OpenAI API
-    log(`New OpenAI prompt: ${prompt}`, 'info');
+    // DEBUG
+    log('Sending payload to OpenAI API...', 'debug');
 
-    const completion = await openAiClient.createCompletion({
+    // Send payload to OpenAI API
+    const completion = await openAiClient.createChatCompletion({
       max_tokens: parseInt(process.env.OPENAI_PARAM_MAX_TOKENS),
       model: process.env.OPENAI_PARAM_MODEL,
-      prompt: prompt,
+      messages: messages,
       temperature: parseFloat(process.env.OPENAI_PARAM_TEMPERATURE),
     });
 
-    let response = completion.data.choices[0].text.trim();
+    // DEBUG
+    log('Payload sent.', 'debug');
 
+    // Assign response
+    response = completion.data.choices[0].message.content.trim();
+
+    // DEBUG
+    log(`OpenAI response: HTTP ${completion.status} (${completion.statusText}) ${util.inspect(response, false, null, true)}`, 'debug');
+
+    // Sometimes an empty response comes back if the prompt is garbage
+    // You can't publish an empty message to Discord's API
     if (response == '' && completion.status == 200) {
       const tryAgainResponses = [
         'I don\'t know what you mean.',
         'Use your words.',
         'That doesn\'t make any sense.',
+        'Could you repeat that, but better?',
       ];
 
+      log(`${util.inspect(response, false, null, true)}`, 'debug');
+
+      // Pick a random tryAgainResponse
       const tryAgainResponse = Math.floor(Math.random() * tryAgainResponses.length);
       response = tryAgainResponses[tryAgainResponse];
     }
 
     // Return OpenAI API response
-    log(`OpenAI response: HTTP ${completion.status} (${completion.statusText}) ${response}`, 'info');
     return response;
   }
   catch (error) {
@@ -98,11 +127,13 @@ function checkEnvironment() {
     'OPENAI_API_KEY',
     'OPENAI_PARAM_MAX_TOKENS',
     'OPENAI_PARAM_MODEL',
+    'OPENAI_PARAM_SYSTEM_PROMPT',
     'OPENAI_PARAM_TEMPERATURE',
   ];
 
   let quitError = false;
 
+  // Check that each required environment variable is set
   requiredEnvVars.forEach(envVar => {
     if (!process.env[envVar]) {
       log(`Environment variable not set: ${envVar}`, 'error');
@@ -110,6 +141,7 @@ function checkEnvironment() {
     }
   });
 
+  // Quit with uncaught error if any environment variable is not set
   if (quitError) {
     log('Environment variables are not configured correctly. See documentation on GitHub.', 'error');
     throw (new Error('Configuration error exit.'));
